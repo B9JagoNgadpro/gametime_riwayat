b1826df47547d8c20a57516e3ebc36a48326335a
belatijagad <belatijagadbintangsyuhada@gmail.com>
2024-04-16 07:16:38 +0000

    hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+pub mod model;
+pub mod repository;
+pub mod service;
+pub mod controller;
+
+use actix_web::{web, App, HttpServer, HttpResponse};
+
+async fn index() -> HttpResponse {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(index))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
