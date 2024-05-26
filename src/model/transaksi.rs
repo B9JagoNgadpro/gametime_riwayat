408ed3f3fce3a558af8b0835abee70989a7d670e
Belati <belatijagadbintangsyuhada@gmail.com>
2024-05-26 13:08:02 +0700

    [GREEN] Implement Transaksi model

--- src/model/transaksi.rs
+++ src/model/transaksi.rs
@@ -1,12 +1,14 @@
 use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 use crate::model::game::Game;
 
+#[derive(Serialize, Deserialize, Debug, PartialEq)]
 pub struct Transaksi {
-    id: Uuid,
-    games: Vec<Game>,
-    total_harga: u64,
-    status_pembayaran: String,
-    tanggal_pembayaran: DateTime<Utc>,
-    pembeli_id: Uuid,
+    pub id: Uuid,
+    pub games: Vec<Game>,
+    pub total_harga: i64,
+    pub status_pembayaran: String,
+    pub tanggal_pembayaran: DateTime<Utc>,
+    pub pembeli_id: Uuid,
 }
