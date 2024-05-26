bd3f7d1e446b0662137014faa0d47ac6821e3058
Belati <belatijagadbintangsyuhada@gmail.com>
2024-05-26 13:48:40 +0700

    [GREEN] Implement Transaction Controller

---
+++ src/controller/transaksi_controller.rs
@@ -0,0 +1,30 @@
+use actix_web::{web, HttpResponse};
+use uuid::Uuid;
+use crate::model::{transaksi::Transaksi, response::Response};
+use crate::service::transaksi_service::TransaksiService;
+
+pub async fn create_transaksi(
+    body: web::Json<Transaksi>,
+    service: web::Data<TransaksiService>,
+) -> HttpResponse {
+    match service.create_transaksi(body.into_inner()).await {
+        Ok(_) => HttpResponse::Ok().json(Response {
+            message: "Created a transaksi with games.".to_string(),
+        }),
+        Err(err) => HttpResponse::InternalServerError().json(Response {
+            message: err,
+        }),
+    }
+}
+
+pub async fn get_user_transactions(
+    user_id: web::Path<Uuid>,
+    service: web::Data<TransaksiService>,
+) -> HttpResponse {
+    match service.get_user_transactions(user_id.into_inner()).await {
+        Ok(transactions) => HttpResponse::Ok().json(transactions),
+        Err(err) => HttpResponse::InternalServerError().json(Response {
+            message: err,
+        }),
+    }
+}
