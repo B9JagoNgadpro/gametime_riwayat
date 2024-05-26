3af2ae1f75c652c009c534d651fb54f1427964e9
Belati <belatijagadbintangsyuhada@gmail.com>
2024-05-26 19:33:01 +0700

    [GREEN] Implement redis integration

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ pub mod model;
 pub mod repository;
 pub mod service;
 pub mod controller;
+pub mod adapter;
 
 use std::env;
 use dotenv::dotenv;
@@ -11,6 +12,7 @@ use repository::transaksi_repository::TransaksiRepository;
 use service::transaksi_service::TransaksiService;
 use sqlx::postgres::{PgPool, PgPoolOptions };
 use crate::controller::transaksi_controller::{create_transaksi, get_user_transactions, get_transaction_game_info_by_penjual};
+use crate::adapter::redis_adapter::RedisAdapter;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
@@ -19,15 +21,17 @@ async fn main() -> std::io::Result<()> {
     let port = port.parse::<u16>().expect("Invalid port given");
 
     let database_url = env::var("DATABASE_URL").expect("Missing database URL");
+    let redis_url = std::env::var("REDIS_URL").expect("REDIS_URL must be set");
 
     let pool: PgPool = PgPoolOptions::new()
         .max_connections(10)
         .connect(&database_url)
         .await
         .unwrap();
+    let redis_adapter = RedisAdapter::new(&redis_url).await;
 
     let repository = TransaksiRepository { pool };
-    let service = web::Data::new(TransaksiService::new(repository));
+    let service = web::Data::new(TransaksiService::new(repository, redis_adapter));
 
     HttpServer::new(move || {
         App::new()
