d4a447cbd14097b7044bf3efd681c374d1583967
Belati <belatijagadbintangsyuhada@gmail.com>
2024-05-26 13:48:55 +0700

    Wrapping up the features

--- src/main.rs
+++ src/main.rs
@@ -4,22 +4,38 @@ pub mod service;
 pub mod controller;
 
 use std::env;
-
-use actix_web::{App, HttpServer};
 use dotenv::dotenv;
+use actix_web::{ web, App, HttpServer };
+use repository::transaksi_repository::TransaksiRepository;
+use service::transaksi_service::TransaksiService;
+use sqlx::postgres::{PgPool, PgPoolOptions };
+use crate::controller::transaksi_controller::{create_transaksi, get_user_transactions};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
-    
     let port = env::var("PORT").expect("Missing port number");
     let port = port.parse::<u16>().expect("Invalid port given");
 
+    let database_url = env::var("DATABASE_URL").expect("Missing database URL");
+
+    let pool: PgPool = PgPoolOptions::new()
+        .max_connections(10)
+        .connect(&database_url)
+        .await
+        .unwrap();
+
+    let repository = TransaksiRepository { pool };
+    let service = web::Data::new(TransaksiService::new(repository));
+
     HttpServer::new(move || {
         App::new()
-            // .app_data(web::Data::new(pool.clone()))
+            .app_data(service.clone())
+            .route("/", web::get().to(|| async { "Hello, World!" }))
+            .route("/create", web::post().to(create_transaksi))
+            .route("/get/{user_id}", web::get().to(get_user_transactions))
     })
-    .bind(("0.0.0.0", port))?
+    .bind(("127.0.0.1", port))?
     .run()
     .await
 }
